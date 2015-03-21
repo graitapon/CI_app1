226c9feb00a0258f14fc4667bb38f6883515a4ee
5710121027-graitapon <gezx750@gmail.com>
2015-03-21 21:23:52 +0700

    CodeIgnitor App

---
+++ application/views/profile_name.php
@@ -0,0 +1,13 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+?><!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<title>Welcome to CodeIgniter</title>
+
+<body>
+    
+    <h1>ไกรทพนธ์ เติมวิทย์ขจร 5710121027</h1>
+</body>
+</html>
---
+++ application/views/profile_show.php
@@ -0,0 +1,18 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+?><!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<title>Welcome to CodeIgniter</title>
+
+<body>
+    
+    <h1>ไกรทพนธ์ เติมวิทย์ขจร 5710121027</h1>
+    <?php
+    echo 'Name: '.$data['name'].'<br>';
+    echo 'LastName: '.$data['lastname'].'<br>';
+
+    ?>
+</body>
+</html>
